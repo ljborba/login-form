d1aa0531b076954c4655ef79463d1bb0f25dc49d
Lucas Janing <janinglucas123@gmail.com>
2022-02-11 22:59:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
+class validator {
+    constructor() {
+        this.validations = [
+
+        ]
+    }
+    // iniciar a validação de todos os campos
+    validate(form) {
+        // pegar os inputs
+    }
+}
+
 let form = document.getElementById("register-form");
 let submit = document.getElementById("btn-submit");
+
+let validator = new validator();
 
 // evento que dispara as validações 
 
 submit.addEventListener("click", function(e) { // enviando o form para o servidor
     e.preventDefault();
-    console.log("Funcionou!");
+    validator.validate(form);
 })
