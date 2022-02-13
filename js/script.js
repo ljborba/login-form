859d1e55ced03abcb5dbfb8880f1f8e25a350f0c
Lucas Janing <janinglucas123@gmail.com>
2022-02-13 00:31:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,32 +4,55 @@ class Validator { // Vakidator com V maiúsculo
             "data-min-length",
         ]
     }
+
     // iniciar a validação de todos os campos
+
     validate(form) {
+
         // pegar os inputs
+
         let inputs = form.getElementsByTagName("input");
+
         // Transformo um HTMLCollection -> Array
+        
         let inputsArray = [...inputs];
+        
         inputsArray.forEach(function(input){
+        
             // Loop em todas as validações existentes
+        
             for(let i = 0 ; this.validations.lenght > i; i++) {
+        
                 if(input.getAttribute(this.validations[i]) != null) {
+        
                     // Limpando a string para se tornar um método
+        
                     let method = this.validations[i].replace("data-", "").replace("-", "");
+        
                     // Valor do input
+        
                     let value = input.getAttribute(this.validations[i]);
+        
                     // Invocar o método
+        
                     this[method](input, value);
                 }
             }
         }, this);
     }
+    
     // Verifica se um input tem um número mínimo de caracteres
+    
     minlength(input, minValue) {
+    
         let inputLength = input.value.length;
+    
         let errorMessage = "O campo precisa ter pelo menos ${minValue} caracteres";
+    
         if(inputLength < minValue) {
+    
             console.log(errorMessage);
+    
         }
     }
 }
