55e13e6dc7b08a66b86a94e00cb17a9d82d74ac2
Lucas Janing <janinglucas123@gmail.com>
2022-02-13 23:17:13 -0300

    1.0

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
+class Validator {
+    constructor() {
+        this.validations = []
+    }
+
+    // Iniciar a validação de todos os campos
+    validate(form) {
+
+        // Pegar os inputs
+        let inputs = form.getElementsByTagName("input");
+
+        // Transformo um HTMLCollection -> Array
+        let inputsArray = [...inputs];
+
+        // Loop nos inputs e validação no que for encontrado
+        inputsArray.forEach(function(input) {
+            console.log(input);
+        });
+    }
+}
+
 let form = document.getElementById("register-form");
 let submit = document.getElementById("btn-submit");
 
 
-// Evento que dispara as validações
+let validator = new Validator();
 
+// Evento que dispara as validações
 submit.addEventListener("click", function(e) {
     e.preventDefault();
+        validator.validate(form);
 });
