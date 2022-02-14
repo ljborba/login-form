7f9b044f7187ab268568e0e43bab811e6a0a48c9
Lucas Janing <janinglucas123@gmail.com>
2022-02-13 21:47:18 -0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let form = document.getElementById("register-form");
+let submit = document.getElementById("btn-submit");
+
+
+// Evento que dispara as validações
+
+submit.addEventListener("click", function(e) {
+    e.preventDefault();
+});
